db8135ebabf4dc98f239cc00655e7d7fd3c05c59
Alibek Omarov <a1ba.omarov@gmail.com>
2013-12-17 20:59:37 +0000

    v 0.1 fixed gitignore

--- build/CMakeFiles/plasma_applet_ponycountdownplasma_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "plasma_applet_ponycountdownplasma_automoc.cpp"
-  "CMakeFiles/plasma_applet_ponycountdownplasma_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/plasma_applet_ponycountdownplasma_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
